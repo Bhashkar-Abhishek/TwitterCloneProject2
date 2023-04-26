41352ac134b2353e11a2e41870c21a3a1b81ccd2
Bhashkar-Abhishek <abhishek.bhashkar42@gmail.com>
2023-04-27 02:40:10 +0530

    registration validation complete

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -8,11 +8,13 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 // import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from "@mui/material/DialogTitle";
+import { useNavigate } from "react-router-dom";
 
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { months } from "./Data";
+import style from "./Registration.module.css"
 const Registration = () => {
   const [open, setOpen] = React.useState(false);
 
@@ -23,7 +25,10 @@ const Registration = () => {
   const [error,setError]=useState(false)
   const [errortxt,setErrorTxt]=useState("")
 
-  const[users,setUsers]=useState([])
+  // const[users,setUsers]=useState([])
+  const navigate=useNavigate()
+  const oldData=JSON.parse(localStorage.getItem("userData"))||[]
+
 
   const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   function isEmail(str) {
@@ -42,25 +47,36 @@ const handleSubmit=(e)=>{
          setErrorTxt("*Invalid email")
   }else if(password.length <6){
     setError(true)
-    setErrorTxt("*Password should be atleast 6 characters")
-  }else{
+    setErrorTxt("*Password should be atleast 6 characters") 
+  }
+  else if(oldData.find((val)=>val.email===email)){
+    setError(true)
+      setErrorTxt(`${email} already exists`)
+    //   setName("")
+    // setEmail("")
+    // setPassword("")
+  }
+  else{
     setError(false)
     alert(`welcome ${name}  SignUp Successfull`)
     setName("")
     setEmail("")
     setPassword("")
+    
 
-    const userInfo={
-      name : name,
-      email : email,
-      password : password
-    }
-    // setUsers(...userInfo)
-//  console.log(users.push(userInfo))
- console.log(users)
+const newUser={
+  name:name,
+  email:email,
+  password:password
+}
 
+const updatedUsers=[...oldData,newUser]
+localStorage.setItem("userData",JSON.stringify(updatedUsers))
+
+navigate("./")
   }
 }
+
 
 
   let dates = Array(31)
@@ -138,18 +154,20 @@ const handleSubmit=(e)=>{
             sx={{ marginTop: "1rem", padding: "0.5rem" }}
             id="outlined-basic"
             label="Password"
+            type="password"
             variant="outlined"
             onChange={(e) => setPassword(e.target.value)}
             value={password}
           />
 
-          <div>
-            <div>
-              <InputLabel id="demo-simple-select-label">DAY</InputLabel>
+          <div className={style.dobContainer}>
+            <div className={style.dayBox}>
+              <InputLabel>DAY</InputLabel>
               <Select
                 // value={Day}
                 label="Day"
                 // onChange={handleChange}
+                
               >
                 {dates.map((date, index) => {
                   return <MenuItem 
@@ -159,7 +177,7 @@ const handleSubmit=(e)=>{
               </Select>
             </div>
             <div>
-              <InputLabel id="demo-simple-select-label">MONTH</InputLabel>
+              <InputLabel>MONTH</InputLabel>
               <Select
                 sx={{ display: "flex" }}
                 // value={Day}
@@ -174,7 +192,7 @@ const handleSubmit=(e)=>{
             </div>
 
             <div>
-              <InputLabel id="demo-simple-select-label">YEAR</InputLabel>
+              <InputLabel>YEAR</InputLabel>
               <Select
                 // value={Day}
                 label="year"
