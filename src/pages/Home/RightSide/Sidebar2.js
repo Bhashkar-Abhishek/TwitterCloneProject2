7e72340c4f497670b7d2158e5dfb9784abbbfb33
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-04-29 23:36:20 +0530

    update home page

--- src/pages/Home/RightSide/Sidebar2.js
+++ src/pages/Home/RightSide/Sidebar2.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import styles from'./Sidebar2.module.css'
+import React from "react";
+import styles from "./Sidebar2.module.css";
+import SearchIcon from "@mui/icons-material/Search";
+import { TwitterShareButton,TwitterTimelineEmbed,TwitterTweetEmbed } from "react-twitter-embed";
 const Sidebar2 = () => {
   return (
     <div className={styles.sidebar2}>
-      <h2>sidebar2</h2>
-    </div>
-  )
-}
+      <div className={styles.side2_input}>
+        <SearchIcon className={styles.side2_searchicon}/>
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className={styles.side2_container}>
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId="1652009110530056192"/>
 
-export default Sidebar2
+        <TwitterTimelineEmbed sourceType="profile" screenName="vikrant"/>
+
+        <TwitterShareButton/>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar2;
