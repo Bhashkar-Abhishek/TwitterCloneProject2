0793834cbb1917a1a7ccd1f1246273299b759181
vikrantkkk <120943926+vikrantkkk@users.noreply.github.com>
2023-04-30 08:00:43 +0530

    tweet box icons added

--- src/pages/Home/Tweet/Tweetbox.js
+++ src/pages/Home/Tweet/Tweetbox.js
@@ -2,7 +2,10 @@ import React from "react";
 import styles from "./Tweetbox.module.css";
 import { Avatar, Button } from "@mui/material";
 import { Images } from "../../../assets/Images";
-
+import { BsCardImage, BsEmojiSmile } from "react-icons/bs";
+import { RiBarChartGroupedFill, RiFileGifLine } from "react-icons/ri";
+import { IoMdCalendar } from "react-icons/io";
+import { MdOutlineLocationOn } from "react-icons/md";
 const Tweetbox = () => {
   return (
     <div className={styles.tweetbox}>
@@ -11,9 +14,16 @@ const Tweetbox = () => {
           <Avatar src={Images} />
           <input placeholder="what's happening ?" type="text" />
         </div>
-        <input className={styles.image_input} placeholder="Enter url?" type="text"/>
         <Button className={styles.tweet_button}>Tweet</Button>
       </form>
+      <div>
+        <BsCardImage />
+        <RiFileGifLine />
+        <RiBarChartGroupedFill />
+        <BsEmojiSmile />
+        <IoMdCalendar />
+        <MdOutlineLocationOn />
+      </div>
     </div>
   );
 };
