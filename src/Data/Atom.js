06f3a6ff91955f290bbf5d34e82a4ff31c072b0a
Bhashkar-Abhishek <abhishek.bhashkar42@gmail.com>
2023-04-30 16:02:38 +0530

    atom

--- src/Data/Atom.js
+++ src/Data/Atom.js
@@ -1,14 +1,7 @@
 import { atom } from "recoil";
 import { Tweets } from "./Data";
-import { Users } from "./Data";
 
 export const tweetData = atom({
   key: "tweetData",
   default: [...Tweets],
 });
-
-
-export const tweetUser = atom({
-    key: "tweetUser",
-    default: [...Users],
-  });
--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,4 +1,4 @@
-export let Tweets = [
+export const Tweets = [
     {
       "id": 1,
       "content": "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
